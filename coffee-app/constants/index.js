fb323926a82bbb2430fa1544732c2170ecc16170
DonMizzy <ejeanobionyeka@gmail.com>
2025-03-27 13:09:33 +0100

    all chnages made since last time

--- coffee-app/constants/index.js
+++ coffee-app/constants/index.js
@@ -35,7 +35,7 @@ export const coffeeItems = [
     id: 2,
     name: "Capuccino",
     price: "15.50",
-    volume: '110ml',
+    volume: '110 ml',
     stars: '4.3',
     image: require('../assets/Capuccino.webp'),
     desc: "The taste of coffee can vary depending on factors such as type of beans, roast"
@@ -44,7 +44,7 @@ export const coffeeItems = [
     id: 3,
     name: "Expresso",
     price: "30.00",
-    volume: '100ml',
+    volume: '100 ml',
     stars: '4.0',
     image: require('../assets/Expresso.webp'),
     desc: "The taste of coffee can vary depending on factors such as type of beans, roast"
@@ -53,7 +53,7 @@ export const coffeeItems = [
     id: 4,
     name: "Latte",
     price: "10.30",
-    volume: '120ml',
+    volume: '120 ml',
     stars: '5.0',
     image: require('../assets/Latte (2).png'),
     desc: "The taste of coffee can vary depending on factors such as type of beans, roast"
@@ -62,7 +62,7 @@ export const coffeeItems = [
     id: 5,
     name: "Mocha",
     price: "10.50",
-    volume: '140ml',
+    volume: '140 ml',
     stars: '4.3',
     image: require('../assets/Mocha.png'),
     desc: "The taste of coffee can vary depending on factors such as type of beans, roast"
@@ -71,7 +71,7 @@ export const coffeeItems = [
     id: 6,
     name: "Americano",
     price: "10.30",
-    volume: '80ml',
+    volume: '80 ml',
     stars: '4.1',
     image: require('../assets/Americano.webp'),
     desc: "The taste of coffee can vary depending on factors such as type of beans, roast"
--- coffee-app/screens/ProductScreen.js
+++ coffee-app/screens/ProductScreen.js
@@ -1,7 +1,15 @@
 import { View, Text, Image, TouchableOpacity } from "react-native";
 import { StatusBar } from "expo-status-bar";
-import { ArrowLeftCircleIcon } from "react-native-heroicons/outline";
-import { HeartIcon, StarIcon } from "react-native-heroicons/solid";
+import {
+  ArrowLeftCircleIcon,
+  ShoppingBagIcon,
+} from "react-native-heroicons/outline";
+import {
+  HeartIcon,
+  StarIcon,
+  MinusIcon,
+  PlusIcon,
+} from "react-native-heroicons/solid";
 import { SafeAreaView } from "react-native-safe-area-context";
 import React, { useState } from "react";
 import { useNavigation } from "@react-navigation/native";
@@ -11,12 +19,14 @@ export default function ProductScreen(props) {
   const item = props.route.params; // we need to get the coffee details from route
   const navigation = useNavigation();
   const [size, setSize] = useState("small");
+  const [digit, setDigit] = useState(2);
   return (
     <View>
       <StatusBar style="light" />
       <Image
         source={require("../images/coffee.jpg")}
-        className="w-full h-[300px] rounded-b-[30px] absolute"
+        className="w-full h-[300px] rounded-b-[30px] absolute opacity-8
+        0"
       />
       <SafeAreaView className="space-y-4">
         <View className="mx-4 flex-row justify-between items-center">
@@ -51,36 +61,104 @@ export default function ProductScreen(props) {
         <View className="mx-4 flex-row justify-between items-center">
           <Text
             style={themeColors.contingentColor}
-            className="text-3xl font-semibold"
+            className="text-3xl font-semibold text-contingentText"
           >
             {item.name}
           </Text>
-          <Text>${item.price}</Text>
+          <Text className="font-semibold text-lg text-contingentText">
+            $ {item.price}
+          </Text>
         </View>
         <View className="mx-4 space-y-2">
-          <Text className="text-lg font-bold">Coffee size</Text>
+          <Text className="text-lg font-bold text-contingentText">
+            Coffee size
+          </Text>
           <View className="flex-row justify-between">
             <TouchableOpacity
               onPress={() => setSize("small")}
-              className={`p-3 px-8 rounded-full ${size=="small" ? 'bg-contingentColor': 'bg-secondary'}`}
+              className={`p-3 px-8 rounded-full ${
+                size == "small" ? "bg-contingentColor" : "bg-secondary"
+              }`}
             >
-              <Text className={size == "small" ? "text-white" : "text-gray-700"}>Small</Text>
+              <Text
+                className={size == "small" ? "text-white" : "text-gray-700"}
+              >
+                Small
+              </Text>
             </TouchableOpacity>
             <TouchableOpacity
-              onPress={() => setSize("small")}
-              className={`p-3 px-8 rounded-full ${size=="small" ? 'bg-contingentColor': 'bg-secondary'}`}
+              onPress={() => setSize("medium")}
+              className={`p-3 px-8 rounded-full ${
+                size == "medium" ? "bg-contingentColor" : "bg-secondary"
+              }`}
             >
-              <Text className={size == "small" ? "text-white" : "text-gray-700"}>Medium</Text>
+              <Text
+                className={size == "medium" ? "text-white" : "text-gray-700"}
+              >
+                Medium
+              </Text>
             </TouchableOpacity>
             <TouchableOpacity
-              onPress={() => setSize("small")}
-              className={`p-3 px-8 rounded-full ${size=="small" ? 'bg-contingentColor': 'bg-secondary'}`}
+              onPress={() => setSize("large")}
+              className={`p-3 px-8 rounded-full ${
+                size == "large" ? "bg-contingentColor" : "bg-secondary"
+              }`}
             >
-              <Text className={size == "small" ? "text-white" : "text-gray-700"}>Large</Text>
+              <Text
+                className={size == "large" ? "text-white" : "text-gray-700"}
+              >
+                Large
+              </Text>
             </TouchableOpacity>
           </View>
+        </View>
+        <View className="mx-4 space-y-2 h-28">
+          <Text className="text-lg font-bold text-contingentText">About</Text>
+          <Text className="text-gray-600">{item.desc}</Text>
+        </View>
+        <View className="flex-row justify-between items-center mx-4 mb-2">
+          <View className="flex-row items-center space-x-1">
+            <Text className="text-base text-gray-500 font-semibold">
+              Volume
+            </Text>
+            <Text className="text-base font-semibold">{item.volume}</Text>
+          </View>
+          <View className="flex-row items-center space-x-4 border-gray-500 border rounded-full p-1 px-4">
+            <TouchableOpacity>
+              <MinusIcon
+                onPress={() => setDigit(digit - 1)}
+                size={20}
+                strokeWidth={3}
+                color={themeColors.contingentText}
+              />
+            </TouchableOpacity>
+            <Text className="font-extrabold text-contingentText text-lg">
+              {digit}
+            </Text>
+            <TouchableOpacity>
+              <PlusIcon
+                onPress={() => setDigit(digit + 1)}
+                size={20}
+                strokeWidth={3}
+                color={themeColors.contingentText}
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
+        {/* buy now button */}
+        <View className="flex-row justify-between items-center mx-4">
+          <TouchableOpacity className="p-4 rounded-full border border-gray-400">
+            <ShoppingBagIcon size="30" color="gray" />
+          </TouchableOpacity>
+          <TouchableOpacity className="bg-contingentColor p-5 rounded-full flex-1 ml-3">
+            <Text className="text-center text-base font-semibold text-white">
+              Buy now
+            </Text>
+          </TouchableOpacity>
         </View>
       </SafeAreaView>
     </View>
   );
 }
+
+//
